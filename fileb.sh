ca21acb84517e1fed51201bbb71acadad2e6f541
zerolovely <45815630+zerolovely@users.noreply.github.com>
2021-06-25 21:03:30 +0800

    Create fileb.sh

---
+++ fileb.sh
@@ -0,0 +1,8 @@
+curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
+cd /etc/systemd/system
+wget https://github.com/zerolovely/mess/raw/master/filebrowser.service
+cd /var/www
+wget https://github.com/zerolovely/mess/raw/master/filebrowserconfig.json
+cd ~
+
+
