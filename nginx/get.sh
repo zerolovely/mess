27498449988d4cbac6854a13f5e1c1a31f0d04ed
zerolovely <45815630+zerolovely@users.noreply.github.com>
2021-07-09 13:47:33 +0800

    Add files via upload

---
+++ nginx/get.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /etc/nginx/GeoLite2
+rm *
+wget https://raw.githubusercontent.com/P3TERX/GeoLite.mmdb/download/GeoLite2-Country.mmdb
+read -p "geoip2.so version:" version
+cd /etc/nginx/modules
+rm *
+wget https://raw.githubusercontent.com/zerolovely/mess/master/nginx/${version}/ngx_http_geoip2_module.so
+cd ~
+curl https://get.acme.sh | sh
+source .bashrc
+acme.sh --set-default-ca  --server  letsencrypt
