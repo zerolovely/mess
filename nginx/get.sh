a40a5dfa1143c78346a7b5c0113fca2f23728934
zerolovely <45815630+zerolovely@users.noreply.github.com>
2021-07-09 13:49:41 +0800

    Update get.sh

--- nginx/get.sh
+++ nginx/get.sh
@@ -4,7 +4,7 @@ rm *
 wget https://raw.githubusercontent.com/P3TERX/GeoLite.mmdb/download/GeoLite2-Country.mmdb
 read -p "geoip2.so version:" version
 cd /etc/nginx/modules
-rm *
+rm ngx_http_geoip2_module.so
 wget https://raw.githubusercontent.com/zerolovely/mess/master/nginx/${version}/ngx_http_geoip2_module.so
 cd ~
 curl https://get.acme.sh | sh
