e3c6e4f8f95a13da4af960c25d9cbdc4f97f0ae7
zerolovely <45815630+zerolovely@users.noreply.github.com>
2021-09-16 14:52:55 +0800

    Update fileb.sh

--- fileb.sh
+++ fileb.sh
@@ -4,5 +4,4 @@ wget https://github.com/zerolovely/mess/raw/master/filebrowser.service
 cd /var/www
 wget https://github.com/zerolovely/mess/raw/master/filebrowserconfig.json
 cd ~
-
-
+systemctl enable filebrowser
