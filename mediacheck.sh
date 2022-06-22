739cab3939f85eec171271cab77c381f46951213
zerolovely <45815630+zerolovely@users.noreply.github.com>
2022-06-22 12:15:30 +0800

    Update mediacheck.sh

--- mediacheck.sh
+++ mediacheck.sh
@@ -6,7 +6,7 @@ install_nfcheck(){
         rm /usr/local/bin/nf
     fi
     nf_version=$(curl -s https://api.github.com/repos/sjlleo/netflix-verify/releases/latest | grep tag_name | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
-    wget -O /usr/local/bin/nf https://github.com/sjlleo/netflix-verify/releases/download/${nf_version}/nf_${nf_version}_linux_${builds}
+    wget -O /usr/local/bin/nf https://github.com/sjlleo/netflix-verify/releases/download/${nf_version}/nf_linux_${builds}
     chmod +x /usr/local/bin/nf
 }
 
@@ -44,7 +44,7 @@ uninstall(){
 main(){
     if [[ $1 == 'install' ]]; then
         install_nfcheck && install_dpcheck && install_tucheck
-        nf -method full && seq -s "=" ${COLUMNS}|sed -E "s/[0-9]//g" && dp && seq -s "=" ${COLUMNS}|sed -E "s/[0-9]//g" && tucheck
+        nf && dp && tucheck
     elif [[ $1 == 'uninstall' ]]; then
         uninstall
     else
