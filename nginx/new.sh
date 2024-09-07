f0776e6c9a8613b326dcbdbc92e4c77692ffc215
zerolovely <45815630+zerolovely@users.noreply.github.com>
2024-09-06 21:07:17 -0700

    Update new.sh

--- nginx/new.sh
+++ nginx/new.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+set -e
 rm /etc/nginx/modules/ngx_http_geoip2_module.so
 read -p "input nginx version:" version
-apt install libpcre3 libpcre3-dev git -y
+apt install libpcre3 libpcre3-dev  libpcre2-dev zlib1g-dev git -y
 wget https://nginx.org/download/nginx-${version}.tar.gz
 git clone https://github.com/leev/ngx_http_geoip2_module.git
 tar xf nginx-${version}.tar.gz
 cd nginx-${version}
 ./configure --with-compat --add-dynamic-module=../ngx_http_geoip2_module/ --with-stream
 make modules
+strip objs/*.so
 cp objs/ngx_http_geoip2_module.so /etc/nginx/modules/
 make clean
 cd ..
